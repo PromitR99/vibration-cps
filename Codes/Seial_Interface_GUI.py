d1c4ad08326362e37bc8227b2ebd23d2720455cb
Promit <pr18u10260@btech.nitdgp.ac.in>
2021-11-02 01:55:51 +0530

    GUI_Begun

---
+++ Codes/Seial_Interface_GUI.py
@@ -0,0 +1,17 @@
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import tkinter as tk
+import numpy as np
+import serial as sr
+import time
+
+#Main_GUI_Code
+root = tk.Tk()
+root.title('Real Time Plot')
+root.configure(background = 'white')
+root.geometry("600x400") #Window Size (in pixels)
+
+#Plot_on_GUI
+
+
+root.mainloop()
